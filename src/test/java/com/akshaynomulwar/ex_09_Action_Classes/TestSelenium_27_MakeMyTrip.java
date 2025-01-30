15e3778ab6d541f3d5948e965004c05e09978e86
Akshay <akn8453@gmail.com>
2025-01-30 21:54:54 +0530

    Added the files of 6th Jan class

---
+++ src/test/java/com/akshaynomulwar/ex_09_Action_Classes/TestSelenium_27_MakeMyTrip.java
@@ -0,0 +1,71 @@
+package com.akshaynomulwar.ex_09_Action_Classes;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class TestSelenium_27_MakeMyTrip {
+
+
+    EdgeDriver driver;
+
+    @BeforeTest
+    public void openBrowser() {
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("---guest");
+        driver = new EdgeDriver((edgeOptions));
+    }
+
+
+    @Description("Verify actions")
+    @Test
+    public void test_actions() throws  InterruptedException {
+        String URL = "https://www.makemytrip.com";
+        driver.get(URL);
+        driver.manage().window().maximize();
+
+        //wait for the popup to come and click the x icon
+        //span[@data-cy="closeModal"]
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.
+                until(ExpectedConditions.
+                        visibilityOfElementLocated(
+                                By.xpath("//span[@data-cy=\"closeModal\"]")));
+
+        driver.findElement(By.xpath("//span[@data-cy=\"closeModal\"]")).click();
+
+        WebElement fromCity = driver.findElement(By.id("fromCity"));
+
+        Actions actions = new Actions(driver);
+        actions.moveToElement(fromCity).sendKeys("del").build().perform();
+
+
+        Thread.sleep(3000);
+        actions.moveToElement(fromCity).keyDown(Keys.ARROW_DOWN).keyDown(Keys.ENTER).perform();
+
+    }
+
+
+        @AfterTest
+    public void closeBrowser() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/akshaynomulwar/ex_10_Windows/TestSelenium_28.java
@@ -0,0 +1,48 @@
+package com.akshaynomulwar.ex_10_Windows;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.util.Set;
+
+public class TestSelenium_28 {
+
+
+    EdgeDriver driver;
+
+    @BeforeTest
+    public void openBrowser() {
+        EdgeOptions edgeOptions = new EdgeOptions();
+        edgeOptions.addArguments("---guest");
+        driver = new EdgeDriver((edgeOptions));
+    }
+
+    @Description("Verify actions")
+    @Test
+    public void test_actions() throws  InterruptedException {
+        String URL = "https://the-internet.herokuapp.com/windows";
+        driver.get(URL);
+        driver.manage().window().maximize();
+
+       String parentWindow = driver.getWindowHandle();
+
+       driver.findElement(By.linkText("Click Here")).click();
+
+       Set<String> windowHandles = driver.getWindowHandles();
+        System.out.println("Window Handles: " + windowHandles);
+
+        for(String handle : windowHandles){
+            driver.switchTo().window(handle);
+            if(driver.getPageSource().contains("New Window"));
+            {
+                System.out.println("Test Passed");
+                break;
+            }
+        }
+
+    }
+}
